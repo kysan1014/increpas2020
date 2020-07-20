a5feee6784d1eff49c8090cb31c926fba7683154
class02 <class02@192.168.0.97>
2020-07-20 13:11:46 +0900

    lecture note

---
+++ class02/src/day03/Test02.java
@@ -0,0 +1,21 @@
+package day03;
+
+import java.util.Scanner;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("이름을 입력하세요 : ");
+		String name = sc.next();
+		
+		System.out.print("나이를 입력하세요 : ");
+		int age = sc.nextInt();
+		
+		System.out.printf("%s : %d", name, age);
+		
+		
+	}
+}
