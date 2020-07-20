f86730ae20bc7312eb1056fc340b1e3445439755
class02 <class02@192.168.0.97>
2020-07-20 14:49:30 +0900

    lecture note

---
+++ class02/src/day03/Test03.java
@@ -0,0 +1,19 @@
+package day03;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int no = 100;
+		float no2 = 3.14f;
+		boolean bool = true;
+		
+		System.out.println("10" + no);
+		System.out.println(no2 + no + "a");
+		System.out.println(no + "10" + no2);
+		System.out.println(no + "" + true);
+		
+	}
+
+}
